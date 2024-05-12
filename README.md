d575b3b94edda46814c8db96281f923df6a62363
christian-byrne <abolkonsky.rem@gmail.com>
2024-05-11 23:15:25 -0700

    LlavaImg2Txt class, README

---
+++ README.md
@@ -0,0 +1,32 @@
+
+## Models Implemented (so far)
+
+- [Salesforce - blip-image-captioning-base](https://huggingface.co/Salesforce/blip-image-captioning-base)
+  - **Title**: BLIP: Bootstrapping Language-Image Pre-training for Unified Vision-Language Understanding and Generation 
+  - **Size**: ~ 1GB
+  - **Dataset**: COCO (The MS COCO dataset is a large-scale object detection, image segmentation, and captioning dataset published by Microsoft)
+- [llava - llava-1.5-7b-hf](https://huggingface.co/llava-hf/llava-1.5-7b-hf)
+  - **Title**: LLava: Large Language Models for Vision and Language Tasks 
+  - **Size**: ~ 15GB
+  - **Dataset**: 558K filtered image-text pairs from LAION/CC/SBU, captioned by BLIP, 158K GPT-generated multimodal instruction-following data, 450K academic-task-oriented VQA data mixture, 40K ShareGPT data.
+  - **Notable Requirements**: transformers >= 4.35.3
+
+## Prompts
+
+Where possible, attempt to get descriptions to follow the format that works well for image generation (based on the assumption that you want to use img2txt as part of workflow in which you use one image or generation as the prompt for another — or something along those lines).
+
+This is the guide for the format of an "ideal" txt2img prompt (using BLIP):
+
+- **Subject** - you can specify region, write the most about the subject
+- **Medium** - material used to make artwork. Some examples are illustration, oil painting, 3D rendering, and photography. Medium has a strong effect because one keyword alone can dramatically change the style.
+- **Style** - artistic style of the image. Examples include impressionist, surrealist, pop art, etc.
+- **Artists**  - Artist names are strong modifiers. They allow you to dial in the exact style using a particular artist as a reference. It is also common to use multiple artist names to blend their styles. Now let’s add Stanley Artgerm Lau, a superhero comic artist, and Alphonse Mucha, a portrait painter in the 19th century.
+- **Website** - Niche graphic websites such as Artstation and Deviant Art aggregate many images of distinct genres. Using them in a prompt is a sure way to steer the image toward these styles.
+- **Resolution** - Resolution represents how sharp and detailed the image is. Let’s add keywords highly detailed and sharp focus
+- **Enviornment**
+- **Additional** Details and objects - Additional details are sweeteners added to modify an image. We will add sci-fi, stunningly beautiful and dystopian to add some vibe to the image.
+- **Composition** - camera type, detail, cinematography, blur, depth-of-field
+- **Color/Warmth** - You can control the overall color of the image by adding color keywords. The colors you specified may appear as a tone or in objects.
+- **Lighting** - Any photographer would tell you lighting is a key factor in creating successful images. Lighting keywords can have a huge effect on how the image looks. Let’s add cinematic lighting and dark to the prompt.
+
+
--- src/test.py
+++ src/test.py
@@ -1,130 +1,182 @@
-import requests
 from PIL import Image
 
 import torch
 from transformers import AutoProcessor, LlavaForConditionalGeneration
 
-model_id = "llava-hf/llava-1.5-7b-hf"
+
+class LlavaImg2Txt:
+    def __init__(self):
+        self.model_id = "llava-hf/llava-1.5-7b-hf"
+
+        self.ideal_prompt_features = {
+            "subject": [
+                "What is the the subject of this?",
+            ],
+            "medium": [
+                "What are the mediums used to make this?",
+            ],
+            "style": [
+                "What are the the artistic styles this is reminiscent of?",
+            ],
+            "mood": [
+                "What is the mood of this?",
+            ],
+            "artists": [
+                "Which famous artists is this reminiscent of?",
+            ],
+            "website": [
+                "What art website was this most likely found on?",
+            ],
+            "resolution": [
+                "How sharp or detailed is this image?",
+            ],
+            "environment": [
+                "What is the environment and background of this image?",
+            ],
+            "location": [
+                "What is the location of this image?",
+            ],
+            "details and objects": [
+                "What are the objects in this image?",
+            ],
+            "colors": [
+                "What is the color palette in this image?",
+            ],
+            "lighting": [
+                "What is the lighting in this image?",
+            ],
+        }
+
+    def generate_caption(self, image: Image):
+        # Convert Image to RGB first
+        if image.getmodebase() != "RGB":
+            image = image.convert("RGB")
+
+        caption, raw_caption = self.__get_caption(image)
+        return caption
+
+    def clean_output(self, decoded_output, delimiter=","):
+        output_only = decoded_output.split("ASSISTANT: ")[1]
+        lines = output_only.split("\n")
+        cleaned_output = ""
+        split_candidates = [
+            "reminiscent of include ",
+            "reminiscent of includes ",
+            "reminiscent of an ",
+            "reminiscent of a ",
+            "reminiscent of ",
+            "giving the scene of ",
+            "giving the scene a ",
+            "appears to be ",
+            "includes a ",
+            "includes an ",
+            "features a ",
+            "image is ",
+            "this is an ",
+            "this is an ",
+            "this is a ",
+            "this is ",
+            "capturing the ",
+            "image of ",
+        ]
+        for line in lines:
+            if line != "":
+                # found_split = False
+                for candidate in split_candidates:
+                    if candidate in line:
+                        line = line.split(candidate)[-1:][0]
+                        # cleaned_output += replace_delimiter(
+                        #     line.split(candidate)[-1:][0], ".", delimiter
+                        # )
+
+                        # found_split = True
+
+                cleaned_output += self.__replace_delimiter(line, ".", delimiter)
+
+        return cleaned_output
+
+    def __get_single_answer_prompt(self, prompt_features):
+        """
+        For multiple turns conversation:
+        "USER: <image>\n<prompt1> ASSISTANT: <answer1></s>USER: <prompt2> ASSISTANT: <answer2></s>USER: <prompt3> ASSISTANT:"
+        From: https://huggingface.co/docs/transformers/en/model_doc/llava#usage-tips
+        Not sure how the formatting works for multi-turn but those are the docs.
+
+        """
+        prompt = "USER: <image>\n"
+        for index, feature in enumerate(prompt_features):
+            if index != 0:
+                prompt += "USER: "
+            prompt += f"{feature} </s >"
+        prompt += "ASSISTANT: "
+
+        return prompt
+
+    def __replace_delimiter(self, text: str, old, new=","):
+        """Replace only the LAST instance of old with new"""
+        if old not in text:
+            return text.strip() + " "
+        last_old_index = text.rindex(old)
+        replaced = text[:last_old_index] + new + text[last_old_index + len(old) :]
+        return replaced.strip() + " "
+
+    def __get_prompt_chunks(self, prompt_features, chunk_size=4):
+        prompt_chunks = []
+        for index, feature in enumerate(prompt_features):
+            if index % chunk_size == 0:
+                prompt_chunks.append([])
+            prompt_chunks[-1].append(prompt_features[feature][0])
+        return prompt_chunks
+
+    def __get_caption(
+        self,
+        raw_image: Image,
+        use_4bit_quantization=True,
+        use_flash2_attention=False,
+        use_low_cpu_mem_usage=True,
+    ):
+        """
+
+        Args:
+            prompt (str): The prompt to generate the caption for
+            use_4bit_quantization (bool): Whether to use 4-bit quantization to reduce memory usage. 4-bit quantization reduces the precision of model parameters, potentially affecting the quality of generated outputs. Use if VRAM is limited.
+            use_flash2_attention (bool): Whether to use Flash-Attention 2. Flash-Attention 2 focuses on optimizing attention mechanisms, which are crucial for the model's performance during generation. Use if computational resources are abundant.
+            use_low_cpu_mem_usage (bool): In low_cpu_mem_usage mode, the model uses less memory on CPU, which can be useful when running multiple models on the same CPU. The model is initialized with optimizations aimed at reducing CPU memory consumption. This can be beneficial when working with large models or limited computational resources, such as systems with low RAM.
+        """
+        model = LlavaForConditionalGeneration.from_pretrained(
+            self.model_id,
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=use_low_cpu_mem_usage,
+            load_in_4bit=use_4bit_quantization,
+            use_flash_attention_2=use_flash2_attention,
+        )
+
+        # model.to() is not supported for 4-bit or 8-bit bitsandbytes models. With 4-bit quantization, use the model as it is, since the model will already be set to the correct devices and casted to the correct `dtype`.
+        if torch.cuda.is_available() and not use_4bit_quantization:
+            model = model.to(0)
+
+        processor = AutoProcessor.from_pretrained(self.model_id)
+        prompt_chunks = self.__get_prompt_chunks(
+            self.ideal_prompt_features, chunk_size=4
+        )
+
+        caption = ""
+        raw_caption = ""
+        for prompt_list in prompt_chunks:
+            prompt = self.__get_single_answer_prompt(prompt_list)
+            inputs = processor(prompt, raw_image, return_tensors="pt").to(
+                0, torch.float16
+            )
+            output = model.generate(**inputs, max_new_tokens=300, do_sample=False)
+            decoded = processor.decode(output[0][2:], skip_special_tokens=True)
+            raw_caption += decoded
+            cleaned = self.clean_output(decoded)
+            caption += cleaned
+
+        return caption, raw_caption
 
 
-ideal_prompt_features = {
-    "subject": [
-        "What is the the subject of this?",
-    ],
-    "medium": [
-        "What are the mediums used to make this?",
-    ],
-    "style": [
-        "What are the the artistic styles this is reminiscent of?",
-    ],
-    "mood": [
-        "What is the mood of this?",
-    ],
-    "artists": [
-        "Which famous artists is this reminiscent of?",
-    ],
-    "website": [
-        "What art website was this most likely found on?",
-    ],
-    "resolution": [
-        "How sharp or detailed is this image?",
-    ],
-}
-
-
-def replace_delimiter(text: str, old, new=","):
-    """Replace only the LAST instance of old with new"""
-    if old not in text:
-        return text.strip() + " "
-    last_old_index = text.rindex(old)
-    replaced = text[:last_old_index] + new + text[last_old_index + len(old) :]
-    return replaced.strip() + " "
-
-
-def clean_output(decoded_output, delimiter=","):
-    print(f"Decoded Output: {decoded_output}")
-    output_only = decoded_output.split("ASSISTANT: ")[1]
-    lines = output_only.split("\n")
-    cleaned_output = ""
-    split_candidates = [
-        "image is ",
-        "this is an ",
-        "this is a ",
-        "this is ",
-        "image of ",
-    ]
-    for line in lines:
-        if line != "":
-            found_split = False
-            for candidate in split_candidates:
-                if candidate in line:
-                    cleaned_output += replace_delimiter(
-                        line.split(candidate)[-1:][0], ".", delimiter
-                    )
-
-                    found_split = True
-                    break
-            if not found_split:
-                cleaned_output += replace_delimiter(line, ".", delimiter)
-
-    print(f"Cleaned Output: {cleaned_output}")
-
-
-def get_single_answer_prompt(prompt_features, question_count=4):
-    """
-    For multiple turns conversation:
-    "USER: <image>\n<prompt1> ASSISTANT: <answer1></s>USER: <prompt2> ASSISTANT: <answer2></s>USER: <prompt3> ASSISTANT:"
-    From: https://huggingface.co/docs/transformers/en/model_doc/llava#usage-tips
-    Not sure how the formatting works for multi-turn but those are the docs.
-
-    """
-    prompt = "USER: <image>\n"
-    for index, feature in enumerate(prompt_features):
-        if index >= question_count:
-            break
-        if index != 0:
-            prompt += "USER: "
-        prompt += f"{prompt_features[feature][0]} </s>"
-    prompt += "ASSISTANT: "
-    return prompt
-
-
-# pic_path = "/media/c_byrne/Seagate Expansion Drive/eastside/nudify-tools/spanish-onlyfans-1/inessa-chimato_0024.jpg"
-
-pic_path = "/home/c_byrne/projects/comfy-node-testing-tools/data/test-images/rgb/movie-scene/rgb-movie-scene-H576px_W1024px-jpg-26.jpg"
-prompt = "USER: <image>\nWhat is the subject of this? </s>USER: What are the mediums used to make this? </s>USER: What are the artistic styles this is reminiscent of? </s>USER: What is the mood of this? </s>ASSISTANT: "
-
-prompt = get_single_answer_prompt(ideal_prompt_features)
-print(prompt)
-
-# Whether to use 4-bit quantization to reduce memory usage. 4-bit quantization reduces the precision of model parameters, potentially affecting the quality of generated outputs. Use if VRAM is limited.
-use_4bit_quantization = True
-# Whether to use Flash-Attention 2. Flash-Attention 2 focuses on optimizing attention mechanisms, which are crucial for the model's performance during generation. Use if computational resources are abundant.
-use_flash2_attention = False
-# In low_cpu_mem_usage mode, the model uses less memory on CPU, which can be useful when running multiple models on the same CPU. The model is initialized with optimizations aimed at reducing CPU memory consumption. This can be beneficial when working with large models or limited computational resources, such as systems with low RAM.
-use_low_cpu_mem_usage = True
-
-model = LlavaForConditionalGeneration.from_pretrained(
-    model_id,
-    torch_dtype=torch.float16,
-    load_in_4bit=use_4bit_quantization,
-    low_cpu_mem_usage=use_low_cpu_mem_usage,
-    use_flash_attention_2=use_flash2_attention,
-)
-
-# model.to() is not supported for 4-bit or 8-bit bitsandbytes models. With 4-bit quantization, use the model as it is, since the model will already be set to the correct devices and casted to the correct `dtype`.
-if torch.cuda.is_available() and not use_4bit_quantization:
-    model = model.to(0)
-
-
-processor = AutoProcessor.from_pretrained(model_id)
-
-
-raw_image = Image.open(pic_path).convert("RGB")
-inputs = processor(prompt, raw_image, return_tensors="pt").to(0, torch.float16)
-
-output = model.generate(**inputs, max_new_tokens=300, do_sample=False)
-
-decoded = processor.decode(output[0][2:], skip_special_tokens=True)
-
-clean_output(decoded)
+if __name__ == "__main__":
+    x = LlavaImg2Txt()
+    test_pic_path = "/home/c_byrne/projects/comfy-node-testing-tools/data/test-images/rgb/movie-scene/rgb-movie-scene-H576px_W1024px-jpg-26.jpg"
+    test_image = Image.open(test_pic_path).convert("RGB")

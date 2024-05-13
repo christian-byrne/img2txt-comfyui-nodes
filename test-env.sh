1087c8d342f111725730a4e06da7d9089f361255
christian-byrne <abolkonsky.rem@gmail.com>
2024-05-12 17:28:01 -0700

    improved script to setup testing environment for different python/os/torch-version/gpu

--- test-env.sh
+++ test-env.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-mkdir test-temp
-cd test-temp
+default_python_version="3.10"
+default_dir_name="temp-test_ev"
 
-python3 -m venv venv
+if [ -z "$1" ]; then
+    python_version="$default_python_version"
+else
+    python_version="$1"
+fi
+
+if [ -z "$2" ]; then
+    dir_name="$default_dir_name"
+else
+    dir_name="$2"
+fi
+
+if [ -d "$dir_name" ]; then
+    echo "Directory $dir_name already exists. Please remove it and try again."
+    exit 1
+fi
+
+mkdir $dir_name
+cd $dir_name
+
+if [ -z "$(command -v pyenv)" ]; then
+    echo "pyenv not found. Please install it and try again."
+    exit 1
+fi
+
+if [ -z "$(command -v git)" ]; then
+    echo "git not found. Please install it and try again."
+    exit 1
+fi
+
+if pyenv versions | grep -q $python_version; then
+    echo "Python $python_version is already installed."
+else
+    pyenv install $python_version
+fi
+pyenv local $python_version
+
+python -m venv venv
 source venv/bin/activate
 
 git clone git@github.com:comfyanonymous/ComfyUI.git
@@ -17,9 +54,13 @@ pip install -r requirements.txt
 cd ../../..
 
 cp ~/tools/sd/sd-interfaces/stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors ComfyUI/models/checkpoints
-cp /home/c_byrne/tools/sd/sd-interfaces/stable-diffusion-webui/models/Stable-diffusion/inpainting/experience_70-inpainting.safetensors ComfyUI/models/checkpoints
+cp ~/tools/sd/sd-interfaces/stable-diffusion-webui/models/Stable-diffusion/inpainting/experience_70-inpainting.safetensors ComfyUI/models/checkpoints
 
-export LD_LIBRARY_PATH="$VIRTUAL_ENV/lib64/python3.10/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH"
+py_ver_no_patch_num=$(echo $python_version | cut -d '.' -f 1,2)
+# py_ver_no_patch_num will look like "3.10"
+
+# export LD_LIBRARY_PATH="$VIRTUAL_ENV/lib64/python3.10/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$VIRTUAL_ENV/lib/python$py_ver_no_patch_num/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH"
 
 xdg-open http://localhost:8188 & disown
 cd ComfyUI
